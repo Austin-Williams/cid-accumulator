8a8ff0adb9c1a2bffee23e04451218afdd6e432f
Austin Williams <midnight@midnight.local>
2025-04-15 17:28:24 -0600

    adjust prompts

--- source/pinner/example.ts
+++ source/pinner/example.ts
@@ -1,16 +1,30 @@
 import { promptUserChoice } from '../shared/userPrompt.js'
+import 'dotenv/config'
 import { Pinner } from './Pinner.js'
 import { ethers } from 'ethers'
 import { getAccumulatorData } from '../shared/accumulator.js'
 
 async function main() {
 	// prompt user for target contract and provider url
-	let contractAddress = await promptUserChoice('Enter the target contract address: ', [], false);
+	let contractAddress = process.env.TARGET_CONTRACT_ADDRESS?.trim();
+	if (!contractAddress) {
+		console.log("No TARGET_CONTRACT_ADDRESS found in environment variables.")
+		contractAddress = await promptUserChoice('Enter the target contract address: ', [], false);
+	} else {
+		console.log(`Using contract address from environment variable: ${contractAddress}`)
+	}
 	if (!(contractAddress && contractAddress.startsWith('0x') && contractAddress.length === 42)) {
 		throw new Error('Invalid Ethereum address.');
 	}
-	let providerUrl = await promptUserChoice("Enter the provider URL (default: 'http://127.0.0.1:8545'): ", [], false);
-	providerUrl = providerUrl.trim() || 'http://127.0.0.1:8545';
+
+	let providerUrl = process.env.RPC_PROVIDER_URL?.trim();
+	if (!providerUrl) {
+		console.log("No RPC_PROVIDER_URL found in environment variables.")
+		providerUrl = await promptUserChoice("Enter the provider URL (default: 'http://127.0.0.1:8545'): ", [], false);
+		providerUrl = providerUrl.trim() || 'http://127.0.0.1:8545';
+	} else {
+		console.log(`Using provider URL from environment variable: ${providerUrl}`)
+	}
 
 	// set up the pinner
 	const provider = new ethers.JsonRpcProvider(providerUrl)
